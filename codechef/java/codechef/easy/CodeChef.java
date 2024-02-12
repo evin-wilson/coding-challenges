273a459ae99f1bb42d4a700b854b5b3815ebc923
Evin Wilson <evinwilsonvd@gmail.com>
2024-02-12 20:59:36 +0530

    solving problems in java

---
+++ codechef/java/codechef/easy/CodeChef.java
@@ -0,0 +1,12 @@
+package codechef.easy;
+
+import java.util.Scanner;
+
+public class CodeChef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        int value1 = scanner.nextInt();
+        System.out.println(value+value1);
+    }
+}
